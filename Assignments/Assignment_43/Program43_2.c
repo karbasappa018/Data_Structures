626b5cdd64309e402799f9875f3611a72a0c6527
karbasappa018 <karbasappatadakale@gmail.com>
2026-03-01 23:09:15 +0530

    Assignment_43 Q2

---
+++ Assignments/Assignment_43/Program43_2.c
@@ -0,0 +1,121 @@
+/*
+    Write a program which displays all prime elements which are prime from singly
+    linear linked list.
+
+    Function Prototype :
+
+    int DisplayPrime( PNODE Head)
+
+    Input linked list : |11|->|28|->|17|->|41|->|6|->|89|
+
+    Output : 11   17  41  89
+
+*/
+
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+#define true 1
+#define false 0
+
+typedef int BOOL;
+
+struct node
+{
+    int Data;
+    struct node* Next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertFirst(PPNODE Head, int No)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->Data = No;
+    newn->Next = NULL;
+
+    if(*Head== NULL)
+    {
+        *Head = newn;
+    }
+    else
+    {
+        newn->Next = *Head;
+        *Head = newn;
+    }
+}
+
+void Display(PNODE Head)
+{
+    while(Head != NULL)
+    {
+        printf("| %d |->",Head->Data);
+        Head = Head->Next;
+    }
+
+    printf("NULL\n");
+}
+
+void DisplayPrime(PNODE Head)
+{
+    while(Head != NULL)
+    {
+        int iCnt = 0;
+        BOOL bFlag = false;
+        
+        if((Head->Data) > 0)
+        {
+            
+
+            for(iCnt = 2; iCnt<= (Head->Data/2); iCnt++)
+            {
+
+                if((Head->Data) %iCnt == 0)
+                {
+                    bFlag = true;
+                }
+                
+            }
+
+            if(bFlag == false)
+            {
+                printf("%d\t",Head->Data);
+            }
+        }
+
+        Head = Head->Next;
+    }
+}
+
+
+
+int main()
+{
+    PNODE First = NULL;
+    
+    InsertFirst(&First,89);
+    InsertFirst(&First,6);
+    InsertFirst(&First,41);
+    InsertFirst(&First,17);
+    InsertFirst(&First,28);
+    InsertFirst(&First,11);
+    
+    Display(First);
+
+    
+    DisplayPrime(First);
+
+    
+
+
+
+
+}
